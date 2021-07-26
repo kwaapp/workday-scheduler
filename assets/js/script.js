fda6270f60acfe349d685b8ef34b40107660115a
kwaapp <kwaapp1016@gmail.com>
2021-07-25 20:02:42 -0400

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,28 @@ var hour16 = moment().hour(16);
 var hour17 = moment().hour(17);
 var hour18 = moment().hour(18);
 
+// Create an array of hour blocks for code generation
+var hours = [hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
+
+// Get local storage data or set to empty
+var events9 = JSON.parse(localStorage.getItem('hour9')) || "";
+var events10 = JSON.parse(localStorage.getItem('hour10')) || "";
+var events11 = JSON.parse(localStorage.getItem('hour11')) || "";
+var events12 = JSON.parse(localStorage.getItem('hour12')) || "";
+var events13 = JSON.parse(localStorage.getItem('hour13')) || "";
+var events14 = JSON.parse(localStorage.getItem('hour14')) || "";
+var events15 = JSON.parse(localStorage.getItem('hour15')) || "";
+var events16 = JSON.parse(localStorage.getItem('hour16')) || "";
+var events17 = JSON.parse(localStorage.getItem('hour17')) || "";
+
+// Generate all timeblocks and insert saved local storage data
+$.each(hours, function (index, value) {
+    events = [events9, events10, events11, events12, events13, events14, events15, events16, events17]
+    $(".container").append("<div class='row'><div class='col-2 hour text-right' id='hour" +
+        (index + 9) + "'><span>" + value.format("h A") + "</span></div><div class='col-8 event-group' id='timeblock" +
+        (index + 9) + "'><textarea class='events col-12' id='eventblock" + (index + 9) + "'>" + events[index] + "</textarea></div>" +
+        "<div class='col-2 save-delete' id='save-delete" + (index + 9) + "'><i class='fas fa-save' title='Save Event'></i> <i class='fas fa-trash' title='Remove Event'></i></div></div></div>");
+});
+
+// Display current day at top of planner
+$("#currentDay").text(currentDay);
